896cec38276c0d7e8d4c59104a835aa54fc97ebb
Iloveupthat-99 <filfmjbever14@gmail.com>
2025-10-01 16:14:16 -0500

    funcion recuperarTexto ok

---
+++ Ejercicio Git/utilitarios/saludo.js
@@ -0,0 +1,10 @@
+saludar=function(){
+    let nombre=recuperarTexto('txtNombre');
+    let apellido=recuperarTexto('txtApellido');
+}
+
+recuperarTexto=function(idComponente){
+    let componente=document.getElementById(idComponente);
+    let valorIngresado=componente.value;
+    return valorIngresado;
+}
