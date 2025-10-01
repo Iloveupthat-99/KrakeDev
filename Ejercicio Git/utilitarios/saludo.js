1dca40c0dc790ad083cc5ff3cba65ed344757285
Iloveupthat-99 <filfmjbever14@gmail.com>
2025-10-01 17:00:49 -0500

    implementó funciones recuperar Int y Float

--- Ejercicio Git/utilitarios/saludo.js
+++ Ejercicio Git/utilitarios/saludo.js
@@ -1,10 +1,26 @@
 saludar=function(){
     let nombre=recuperarTexto('txtNombre');
     let apellido=recuperarTexto('txtApellido');
+    let edad=recuperarInt('txtEdad');
+    let estatura=recuperarFloat('txtEstatura');
 }
 
 recuperarTexto=function(idComponente){
     let componente=document.getElementById(idComponente);
     let valorIngresado=componente.value;
     return valorIngresado;
+}
+
+recuperarInt=function(idComponente){
+    let componente=document.getElementById(idComponente);
+    let valorIngresado=componente.value;
+    let valorInt=parseInt(valorIngresado);
+    return valorInt;
+}
+
+recuperarFloat=function(idComponente){
+    let componente=document.getElementById(idComponente);
+    let valorIngresado=componente.value;
+    let valorFloat=parseFloat(valorIngresado);
+    return valorFloat;
 }
