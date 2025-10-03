29ad2ef57a04e8244a27c48492f42224902c65f4
Iloveupthat-99 <filfmjbever14@gmail.com>
2025-10-03 18:35:40 -0500

    Se separa las funciones al archivo utilitarios.js

--- Ejercicio Git/utilitarios/saludo.js
+++ Ejercicio Git/utilitarios/saludo.js
@@ -8,38 +8,3 @@ saludar=function(){
     mostrarImagen('niceGif', './imagenes/thatsNice.gif')
     mostrarTextoEnCaja('txtNombre', ' ');
 }
-
-mostrarTextoEnCaja=function(idComponente, mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.value=mensaje;
-}
-
-mostrarTexto=function(idComponente, mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.innerText=mensaje;
-}
-
-mostrarImagen=function(idComponente, rutaImagen){
-    let componente=document.getElementById(idComponente);
-    componente.src=rutaImagen;
-}
-
-recuperarTexto=function(idComponente){
-    let componente=document.getElementById(idComponente);
-    let valorIngresado=componente.value;
-    return valorIngresado;
-}
-
-recuperarInt=function(idComponente){
-    let componente=document.getElementById(idComponente);
-    let valorIngresado=componente.value;
-    let valorInt=parseInt(valorIngresado);
-    return valorInt;
-}
-
-recuperarFloat=function(idComponente){
-    let componente=document.getElementById(idComponente);
-    let valorIngresado=componente.value;
-    let valorFloat=parseFloat(valorIngresado);
-    return valorFloat;
-}
---
+++ Ejercicio Git/utilitarios/utilitarios.js
@@ -0,0 +1,34 @@
+mostrarTextoEnCaja=function(idComponente, mensaje){
+    let componente=document.getElementById(idComponente);
+    componente.value=mensaje;
+}
+
+mostrarTexto=function(idComponente, mensaje){
+    let componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
+}
+
+mostrarImagen=function(idComponente, rutaImagen){
+    let componente=document.getElementById(idComponente);
+    componente.src=rutaImagen;
+}
+
+recuperarTexto=function(idComponente){
+    let componente=document.getElementById(idComponente);
+    let valorIngresado=componente.value;
+    return valorIngresado;
+}
+
+recuperarInt=function(idComponente){
+    let componente=document.getElementById(idComponente);
+    let valorIngresado=componente.value;
+    let valorInt=parseInt(valorIngresado);
+    return valorInt;
+}
+
+recuperarFloat=function(idComponente){
+    let componente=document.getElementById(idComponente);
+    let valorIngresado=componente.value;
+    let valorFloat=parseFloat(valorIngresado);
+    return valorFloat;
+}
