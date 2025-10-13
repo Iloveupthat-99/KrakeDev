d57aacd459891dc36644f6a4b97717c0522d8bd1
Iloveupthat-99 <filfmjbever14@gmail.com>
2025-10-13 15:49:32 -0500

    Gifs aniadidos

--- Ejercicio Git/promedioNotas/promedioNotas.js
+++ Ejercicio Git/promedioNotas/promedioNotas.js
@@ -5,4 +5,9 @@ calcularPromedioNotas=function(){
     let promedio=calcularPromedio(nota1, nota2, nota3);
     let promedioFxd=promedio.toFixed(2);
     mostrarTexto('promedio', promedioFxd);
+    if(promedioFxd > 7){
+        mostrarImagen('promedioGif', './imagenes/happyBears.gif');
+    }else{
+        mostrarImagen('promedioGif', './imagenes/extrSad.gif');
+    }
 }
