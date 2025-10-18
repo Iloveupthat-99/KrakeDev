d0ec90095fd2ba958e4572ac8f939930e5214fdb
Iloveupthat-99 <filfmjbever14@gmail.com>
2025-10-17 21:31:54 -0500

    PromedioNotas2 cambiado condicionales

--- Ejercicio Git/promedioNotas/promedioNotas.js
+++ Ejercicio Git/promedioNotas/promedioNotas.js
@@ -5,9 +5,17 @@ calcularPromedioNotas=function(){
     let promedio=calcularPromedio(nota1, nota2, nota3);
     let promedioFxd=promedio.toFixed(2);
     mostrarTexto('promedio', promedioFxd);
-    if(promedioFxd > 7){
-        mostrarImagen('promedioGif', './imagenes/happyBears.gif');
+    if(promedioFxd < 5 && promedioFxd > 0){
+        mostrarImagen('promedioGif', './imagenes/going.gif');
+        mostrarTexto('mensaje', 'REPROBADO');
+    }else if(promedioFxd >=5 && promedioFxd <=8){
+        mostrarImagen('promedioGif', './imagenes/nice.gif');
+        mostrarTexto('mensaje', 'BUEN TRABAJO');
+    }else if(promedioFxd > 8 && promedioFxd <=10){
+        mostrarImagen('promedioGif', './imagenes/amaziiing.gif');
+        mostrarTexto('mensaje', 'EXCELENTE');
     }else{
-        mostrarImagen('promedioGif', './imagenes/extrSad.gif');
+        mostrarImagen('promedioGif', './imagenes/oops.gif');
+        mostrarTexto('mensaje', 'DATOS INCORREECTOS');
     }
 }
