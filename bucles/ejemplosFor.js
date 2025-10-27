536583e5bfb774aaf681ce9f4e166db8de8b08ad
Iloveupthat-99 <filfmjbever14@gmail.com>
2025-10-27 16:13:26 -0500

    Ejercicio for terminado, adicional removido documento word anterior ejercicio

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,44 @@
+mostrarNumeros=function(){
+    console.log('antes del for');
+    for(let i=0;i<4;i++){
+        console.log(i);
+    }
+    console.log('despues del for');
+}
+mostrarNumeros2=function(){
+    console.log('antes del for');
+    for(let indice=1;indice<=5;indice++){
+        console.log(indice);
+    }
+    console.log('despues del for');
+
+}
+mostrarPares=function(){
+    console.log('antes del for');
+    for(let pares = 2;pares <= 10;pares += 2){
+        console.log(pares);
+    }
+    console.log('despues del for');
+}
+mostrarInversa=function(){
+    console.log('antes del for');
+    for(let i = 10;i >= 0;i--){
+        console.log(i);
+    }
+    console.log('despues del for');
+}
+hackearNasaPelis=function(){
+    //hackeando NASA 0%....100%
+    console.log('antes del for');
+    for(let porcentaje = 0;porcentaje <= 100;porcentaje += 5){
+        console.log('hackeando NASA '+porcentaje+'%');
+    }
+    console.log('La NASA ha sido hackeada xD');
+}
+mostrarImpares=function(){
+    console.log('antes del For');
+    for(let impares=1;impares<=21;impares+=2){
+        console.log(impares);
+    }
+    console.log('despues del For');
+}
