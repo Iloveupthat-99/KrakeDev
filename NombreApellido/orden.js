eb6422a301b5d42491555294f035ef2092fad573
Iloveupthat-99 <filfmjbever14@gmail.com>
2025-11-24 16:19:31 -0500

    Parte 1 terminada

---
+++ NombreApellido/orden.js
@@ -0,0 +1,25 @@
+personas =[
+    {nombre:'Marcos', edad:18},
+    {nombre:'Roberto', edad:15},
+    {nombre:'Kate', edad:25},
+    {nombre:'Diana', edad:12},
+    {nombre:'Benja', edad:5}
+]
+
+agregarPersona=function(){
+    let name=recuperarTexto('txtNombre');
+    let age;
+    if(name>2){    
+    age=recuperarTexto('txtApellido');
+    }else{
+        mostrarTexto('lblError1', 'DEBE CONTENER MINIMO 3 CARACTERES');
+    }
+    if(age<0&&age>101){
+        mostrarTexto('lblError2', 'DEBE CONTENER NUMEROS ENTRE 0 Y 100');
+    }else{
+        nuevaPersona={nombre:name, edad:age};
+        personas.push(nuevaPersona)
+        alert('Persona agregada correctamente');
+    }
+    
+}
